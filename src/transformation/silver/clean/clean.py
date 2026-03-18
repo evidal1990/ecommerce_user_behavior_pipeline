07f8daa79f150db917715f51921b2667fec54414
Edith Vidal <ecsr.vidal@gmail.com>
2026-03-18 16:06:20 -0300

    Preenchendo colunas string com Missing quando for null

--- src/transformation/silver/clean/clean.py
+++ src/transformation/silver/clean/clean.py
@@ -2,7 +2,7 @@ import polars as pl
 import logging
 from .format import FormatData
 from .remove_duplicates import RemoveDuplicates
-from .fill_numeric_columns import FillNumericColumns
+from .fill_columns import FillColumns
 
 
 class CleanData:
@@ -17,6 +17,6 @@ class CleanData:
         df = FormatData().execute(df=df)
         logging.info("Formatação de strings finalizada")
 
-        df = FillNumericColumns().execute(df=df)
+        df = FillColumns().execute(df=df)
         logging.info("Tratamento de dados numéricos finalizado")
         return df
---
+++ src/transformation/silver/clean/fill_columns.py
@@ -0,0 +1,116 @@
+import logging
+import polars as pl
+
+
+class FillColumns:
+
+    def __init__(self) -> None:
+        pass
+
+    def execute(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        df_cleaned = self._fill(df=df)
+        logging.info(
+            (
+                f"DATA_CLEANING_FILL_EMPTY_REGISTRIES\n"
+                f"Registros: {df_cleaned.height}\n"
+                f"Registros inválidos antes da limpeza:\n"
+                f"{self._invalid_registries(df=df)}\n"
+                f"Registros inválidos depois da limpeza:\n"
+                f"{self._invalid_registries(df=df_cleaned)}\n"
+            )
+        )
+
+        return df
+
+    def _invalid_registries(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        pl_int = (
+            (pl.col(pl.Int64).is_null())
+            .sum()
+            .name.keep()
+        )
+        pl_float = (
+            (pl.col(pl.Float64).is_null() | pl.col(pl.Float64).is_nan())
+            .sum()
+            .name.keep()
+        )
+        pl_string = (
+            (pl.col(pl.String).is_null() | (pl.col(pl.String).str.strip_chars() == ""))
+            .sum()
+            .name.keep()
+        )
+        return (
+            df.select(
+                [
+                    pl_int,
+                    pl_float,
+                    pl_string,
+                ]
+            )
+            .unpivot(variable_name="coluna", value_name="invalidos")
+            .filter(pl.col("invalidos") > 0)
+        )
+
+    def _fill(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        strategies = {
+            pl.Float64: lambda col: self._fill_numeric(
+                df=df,
+                col=col,
+            ),
+            pl.Int64: lambda col: self._fill_numeric(
+                df=df,
+                col=col,
+            ),
+            pl.String: lambda col: self._fill_literal(
+                df=df,
+                col=col,
+            ),
+        }
+        exprs = map(
+            lambda item: strategies.get(
+                item[1],
+                lambda column: pl.col(
+                    column,
+                ),
+            )(
+                item[0]
+            ).alias(item[0]),
+            df.schema.items(),
+        )
+        return df.with_columns(list(exprs))
+
+    def _fill_numeric(
+        self,
+        df: pl.DataFrame,
+        col: str,
+    ) -> pl.Expr:
+        return (
+            pl.when(pl.col(col) < 0)
+            .then(None)
+            .otherwise(pl.col(col))
+            .fill_nan(None)
+            .fill_null(
+                df.select(
+                    pl.col(col).fill_nan(None).filter(pl.col(col) >= 0).median()
+                ).item()
+            )
+        )
+
+    def _fill_literal(
+        self,
+        col: str,
+    ) -> pl.Expr:
+        return (
+            pl.col(col)
+            .str.strip_chars()
+            .pipe(lambda col: col.replace("", None))
+            .fill_null("Missing")
+        )
