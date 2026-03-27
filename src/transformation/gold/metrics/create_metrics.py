8e064d44934dbc9cdc4715dcd71aa2f4a110015c
Edith Vidal <ecsr.vidal@gmail.com>
2026-03-27 19:37:57 -0300

    Refatorando percentage_structure

--- src/transformation/gold/metrics/create_metrics.py
+++ src/transformation/gold/metrics/create_metrics.py
@@ -5,7 +5,9 @@ from .kpis.descriptive.users_by_country import PercentageUsersByCountry
 from .kpis.descriptive.users_by_urban_rural import PercentageUsersByUrbanRural
 from .kpis.descriptive.users_by_annual_income import PercentageUsersByAnnualIncome
 from .kpis.descriptive.users_by_education_level import PercentageUsersByEducationLevel
-from .kpis.descriptive.users_by_employment_status import PercentageUsersByEmploymentStatus
+from .kpis.descriptive.users_by_employment_status import (
+    PercentageUsersByEmploymentStatus,
+)
 from .kpis.descriptive.users_by_device_type import PercentageUsersByDeviceType
 from .kpis.descriptive.users_by_has_children import PercentageUsersByHasChildren
 
@@ -13,13 +15,15 @@ from .kpis.descriptive.users_by_has_children import PercentageUsersByHasChildren
 class CreateMetrics:
 
     def __init__(self) -> None:
-        self.df = None
+        pass
 
     def execute(
         self,
         df: pl.DataFrame,
     ) -> pl.DataFrame:
         kpis = []
+
+        # Descriptive
         kpis.append(PercentageUsersByAgeGroup().calculate(df=df))
         kpis.append(PercentageUsersByGender().calculate(df=df))
         kpis.append(PercentageUsersByCountry().calculate(df=df))
@@ -29,5 +33,11 @@ class CreateMetrics:
         kpis.append(PercentageUsersByEmploymentStatus().calculate(df=df))
         kpis.append(PercentageUsersByDeviceType().calculate(df=df))
         kpis.append(PercentageUsersByHasChildren().calculate(df=df))
+
+        # Behavioural
+
+        # Operational
+
+        # Strategic
 
         return pl.concat(kpis)
--- src/transformation/gold/metrics/percentage_structure.py
+++ src/transformation/gold/metrics/percentage_structure.py
@@ -12,20 +12,31 @@ class PercentageStructure:
         self,
         df: pl.DataFrame,
     ) -> pl.DataFrame:
-        return (
-            df.group_by(self.column)
-            .agg((pl.count() * 100 / df.height).round(2).alias("percentage"))
-            .with_columns(
-                [
-                    pl.lit(self.column).cast(pl.Utf8).alias("dimension"),
-                    pl.col(self.column).cast(pl.Utf8).alias("value"),
-                ]
-            )
-            .select(
-                [
-                    "dimension",
-                    "value",
-                    "percentage",
-                ]
-            )
+        df_agg = self._calculate_percentage(df)
+        df_enriched = self._add_dimension_and_value(df_agg)
+        return self._format_output(df_enriched)
+
+    def _calculate_percentage(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        return df.group_by(self.column).agg(
+            (pl.count() * 100 / df.height).round(2).alias("percentage")
         )
+
+    def _add_dimension_and_value(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        return df.with_columns(
+            [
+                pl.lit(self.column).cast(pl.Utf8).alias("dimension"),
+                pl.col(self.column).cast(pl.Utf8).alias("value"),
+            ]
+        )
+
+    def _format_output(
+        self,
+        df: pl.DataFrame,
+    ) -> pl.DataFrame:
+        return df.select(["dimension", "value", "percentage"])
